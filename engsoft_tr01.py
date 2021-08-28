fe98b2ce59960e8d622edf0742d6d0b914a45e27
jonnahfreire <jonnas.freire17@gmail.com>
2021-08-28 15:52:46 -0300

    Added import sum of the engsoft_tr01 module

---
+++ engsoft_tr01.py
@@ -0,0 +1,13 @@
+print("Testing git and github interaction...")
+
+
+def sum(n1, n2):
+    return n1 + n2
+
+
+def main():
+    print("Sum of 3 + 6 = ", sum(3, 6))
+
+
+if __name__ == "__main__":
+    main()
---
+++ engsoft_tr02.py
@@ -0,0 +1,12 @@
+from engsoft_tr01 import sum
+
+
+print("Hello, Software engineering world!!")
+
+
+def main():
+    print(sum(3, 6))
+
+
+if __name__ == "__main__":
+    main()
